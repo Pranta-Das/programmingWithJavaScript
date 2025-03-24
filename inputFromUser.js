a2ecb3727e82fd50bacd8c4499813127bfb7dac4
Pranta-Das <prantodas423@gmail.com>
2025-03-24 20:23:47 +0600

    maximum number between three value

--- inputFromUser.js
+++ inputFromUser.js
@@ -2,13 +2,18 @@ const prompt=require("prompt-sync")({sigint:true});
 
 let num1 = parseInt(prompt("enter the first value : "));
 let num2 = parseInt(prompt("enter the second value : "));
+let num3 = parseInt(prompt("enter the third value : "));
 console.log(num1+num2);
-if(num1> num2)
+if(num1> num2 && num1>num3)
 {
     console.log("the maximum value is : ", num1);
 }
-else{
+else if(num2>num3)
+    {
     console.log("Maximum value is : ", num2);
-}
+    }
+    else{
+        console.log("maximum number is : ", num3);
+    }
 
 
