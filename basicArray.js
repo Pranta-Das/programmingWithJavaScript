28b94db885202a7f1f5d1eb2c9a34acd082a6d59
Pranta-Das <prantodas423@gmail.com>
2025-03-26 23:43:55 +0600

    array push and pop method

--- basicArray.js
+++ basicArray.js
@@ -7,3 +7,18 @@ console.log(arrayIndex);
 // update index value
 arrayOne[4] = 299;
 console.log(arrayOne);
+
+// For adding some value in Array we have to use Push.
+//If we use push then value will be added on the last of the array. 
+
+arrayTow = ['Joy', 'Bijoy', 'raju', 'riju', "salam"];
+console.log(arrayTow);
+arrayTow.push('Nisad');
+console.log(arrayTow);
+
+//Now for removing any value mainly last value from the array we have to use arrayName.pop().
+
+const arrayNumber = ['ami', 'Tumi ', 'sei'];
+const removeLastValue = arrayNumber.pop();
+console.log(removeLastValue);
+console.log(arrayNumber);
