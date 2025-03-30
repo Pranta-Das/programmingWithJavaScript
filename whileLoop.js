109ae4ac76ba29a55dab9d481df0ad52bb9fff42
Pranta-Das <prantodas423@gmail.com>
2025-03-30 16:04:56 +0600

    loop-2

--- whileLoop.js
+++ whileLoop.js
@@ -1,5 +1,14 @@
+const prompt = require('prompt-sync')({sigint: true});
 let num = 1;
 while(num<5){
     console.log(num,'. Bangladesh')
     num++;
 }
+
+let s= 0;
+let a= 1;
+const number = prompt('Enter one integer number: ');
+while(number>=a){
+    s=s+a;
+    a++;
+}console.log('The sum is: ', s);
